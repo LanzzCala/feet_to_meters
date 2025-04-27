e7eca06a585c84061005e92b3f7cd61ed26b1e24
Lanzz Laptop <calap.dev@gmail.com>
2025-04-26 18:18:31 -0700

    Create labels and button for GUI

--- feet_to_meters.py
+++ feet_to_meters.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-root.title("Feet to Meters")
+root.title("Feet to Meters generator")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky= (N, W, E, S))
@@ -16,7 +16,11 @@ feet_entry.grid(column=2, row=1, sticky=(W, E))
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
-ttk.Button(mainframe, text = "Calculate", command=)
+ttk.Button(mainframe, text = "Calculate").grid(column=3, row=3, sticky=(W))
+
+ttk.Label(mainframe, text="Feet").grid(column=3, row=1, sticky=(W))
+ttk.Label(mainframe, text="is equal to").grid(column=1, row=2, sticky=(E))
+ttk.Label(mainframe, text= "meters").grid(column=3, row=2, sticky=(W))
 
 
 root.mainloop()
